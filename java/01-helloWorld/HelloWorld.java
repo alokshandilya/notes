c8807232226507ba8c86f7971993bef3529cd5a2
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-05 01:31:24 +0530

    add HelloWorld
    - print() vs println()

---
+++ java/01-helloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    /*
+      System.out.print() does not add line break at end
+      System.out.println() does adds line brak at end
+    */
+
+    System.out.print("Hello");
+    System.out.print("World");
+    System.out.println("Hello ");
+    System.out.println("World");
+  }
+}
