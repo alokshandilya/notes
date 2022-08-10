ab4aa9703d1017c71b9146c507ff1b75e46f3c0e
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-11 04:31:31 +0530

    update ReverseOfNumber.java
    - store reversed number

--- java/05-loops-flowControl/ReverseOfNumber.java
+++ java/05-loops-flowControl/ReverseOfNumber.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 // last digit = num % 10
-// remove last digit = num / 10
+// remove last digit = num / 10 [update num]
 public class ReverseOfNumber {
   public static void main(String[] args) {
     System.out.print("Enter a number : ");
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
+    int number = sc.nextInt();
     sc.close();
-    while (num > 0) {
-      int lastDigit = num % 10;
-      num /= 10;
+    int reversedNumber = 0;
+
+    while (number > 0) {
+      int lastDigit = number % 10;
+      reversedNumber = (reversedNumber * 10) + lastDigit;
+      number /= 10;
       System.out.print(lastDigit);
     }
     System.out.println();
