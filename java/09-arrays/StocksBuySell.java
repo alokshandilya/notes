4f4dfe25676cefe079144b5cdfd381f371d7843b
Alok Shandilya <alok.shandilya10@gmail.com>
2022-08-29 18:30:31 +0530

    add leetcode URL

--- java/09-arrays/StocksBuySell.java
+++ java/09-arrays/StocksBuySell.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
 // you are given an array prices where prices[i] is the price of a given stock on the ith day. you
 // want to maximize your profit by choosing a single day to buy one stock and choosing a different
 // day in the future to sell that stock. return the maximum profit you can achieve from this
